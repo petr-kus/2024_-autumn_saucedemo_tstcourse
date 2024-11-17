f4e062fa601ed1b1e8d7491835807926cbf72386
AntoninFaktor <167860844+AntoninFaktor@users.noreply.github.com>
2024-11-17 11:47:03 +0100

    TAC-31 - Setting environment, creating AT
    TAC-31
    1) created environment
    2) created install_dependencies script
    3) created requirements doc
    4) created automated checkout text

---
+++ ucastnici/Antonin/install_dependencies.ps1
@@ -0,0 +1,4 @@
+python.exe -m pip install --upgrade pip   
+.\venv\Scripts\Activate.ps1
+pip install -r requirements.txt
+pip freeze > requirements.txt  
---
+++ ucastnici/Antonin/test_checkout.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import time
+import random
+from selenium.webdriver.chrome.options import Options
+
+Option = Options()
+Option.add_argument("start-maximized")
+driver=webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+#--->login page
+#fill user name
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+#fill password
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+#login
+driver.find_element(By.ID, "login-button").click()
+
+#--->inventory page
+#add rnd item to cart
+AtCs=driver.find_elements(By.CLASS_NAME, "btn_primary")
+AtCs[random.randrange(1,len(AtCs))].click()
+#enter the cart
+driver.find_element(By.ID, "shopping_cart_container").click()
+
+#--->cart page
+#check for items in the cart
+cartItems = driver.find_elements(By.CLASS_NAME, "cart_item")
+assert len(cartItems) > 0
+#continue with checkout
+driver.find_element(By.ID, "checkout").click()
+
+#--->1st checkout step
+#fill first name
+driver.find_element(By.ID, "first-name").send_keys("John")
+#fill last name
+driver.find_element(By.ID, "last-name").send_keys("Doe")
+#fill postalCode
+driver.find_element(By.ID, "postal-code").send_keys("111")
+#continue with checkout
+driver.find_element(By.ID, "continue").click()
+
+#--->2nd checkout step
+#check for items in order
+checkoutItems = driver.find_elements(By.CLASS_NAME, "cart_item")
+assert len(checkoutItems) > 0
+#finish the checkout
+driver.find_element(By.ID, "finish").click()
+
+#--->confirmation page
+#validate expected URL
+assert driver.current_url == "https://www.saucedemo.com/checkout-complete.html"
+
+time.sleep(5)
+driver.close()
