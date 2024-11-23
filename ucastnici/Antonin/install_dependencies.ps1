c3d9763eee0b597292380e7e0cc3037ef60bed40
AntoninFaktor <167860844+AntoninFaktor@users.noreply.github.com>
2024-11-23 16:14:10 +0100

    Refactored checkout test
    TAC-31: Uploaded refactored checkout test.

--- ucastnici/Antonin/install_dependencies.ps1
+++ ucastnici/Antonin/install_dependencies.ps1
@@ -1,8 +1,5 @@
 python.exe -m pip install --upgrade pip
 
-# Lektor: zde chybi vytvoreni virtualniho enviromentu 
-#(nezminil jsem na lekci priomo kod - ale pozornemu pozorovateli to mohlo i dojit)
-# doplnuji... probereme na lekci
 python -m venv venv
 
 .\venv\Scripts\Activate.ps1
