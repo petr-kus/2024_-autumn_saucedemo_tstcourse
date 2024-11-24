dad7c444ab373922dcf08262628d07bc6a3c3d89
Petros841 <kastanek.brno@seznam.cz>
2024-11-24 21:07:08 +0100

    Create test-script.py

---
+++ ucastnici/Petr_Sistek/test-script.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import time
+from selenium.webdriver.chrome.options import Options
+
+Option = Options()
+Option.add_argument("start-maximized")
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+username = driver.find_element(By.ID,'user-name')
+username = driver.find_element(By.ID,'user-name')
+username.send_keys('standard_user') 
+password = driver.find_element(By.ID,'password')
+password.send_keys('secret_sauce')
+login_button = driver.find_element(By.ID, 'login-button')
+login_button.click()
+time.sleep(5)
+driver.close()
