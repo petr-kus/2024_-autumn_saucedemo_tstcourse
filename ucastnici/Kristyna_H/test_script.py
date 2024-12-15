0380959a218d3292ac965b1a6f5b88b0bab9d65f
Petr Kus <petr.kus@thermofisher.com>
2024-12-15 22:19:48 +0100

    Lektor Ukol 5 - Kristyna

--- ucastnici/Kristyna_H/test_script.py
+++ ucastnici/Kristyna_H/test_script.py
@@ -5,13 +5,19 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.chrome.options import Options
 
+#TODO Lektor - Komentovano na lekci tak, z rychliku...
+#TODO Lektor - Super je ze skript jede a selhava s error_user do konzole!
+
 Option = Options()
 Option.add_argument("start-maximized")
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 
 username = driver.find_element(By.ID, "user-name")
+#TODO Lektor - ID melo byt oddeleno nekam jinam do promene treba nahoru...
 username.send_keys("standard_user")
+
+#TODO Lektor - mohl byt one liner s oddelenim do dictionary! driver.find_element(*username).send_keys(user['name'])
 
 password = driver.find_element(By.ID, "password")
 password.send_keys("secret_sauce")
@@ -32,6 +38,8 @@ items_in_cart = driver.find_elements(By.CLASS_NAME, "cart_item")
 expected_item_count = 1
 actual_item_count = len(items_in_cart)
 assert actual_item_count == expected_item_count, f"Expected item count is one, actual item count is {actual_item_count}"
+#TODO Lektor - supoer ze je tu assert a ze krasne logujes...
+
 checkout_button = driver.find_element(By.ID, "checkout")
 checkout_button.click()
 
@@ -56,4 +64,5 @@ expected_final_message = "Thank you for your order!"
 assert actual_final_message == expected_final_message, f"Expected message is {expected_final_message}, actual message is {actual_final_message}"
 
 time.sleep(10)
+#TODO Lektor - to je moc na tvrdo... mohlo to byt i nekde v prubehu testu a navrchu parametrizovany...
 driver.close()
