d8e390f7ec963f913c4347a39c20d63983ed21a8
Petr Kus <petr.kus@thermofisher.com>
2024-12-22 14:36:49 +0100

    DU 7 - oprava - Antonin - prehnane POM/OOP - dobra myslenka s tridou pro policka a talcitka (jen uplatneno na spatne veci)

--- ucastnici/Antonin/Assets/tests.py
+++ ucastnici/Antonin/Assets/tests.py
@@ -7,6 +7,11 @@ def setup():
 def loginTest():
     loginPage.fillUserName
     loginPage.fillPassword
+    #TODO - Lektor: krasne POM/OOP! pokud ale ty data zapadly takto moc... pak pouzit fillValidUserName a nebo to zde pouzit jako parametr... cely test muze byt parametrizovan... user, password, valid...
+    #moc jsi veci rozbil a moc je schoval. Chces obecnost ale zaroven mit vse na jednom miste co to jde... ty jsi moc zafocusoval to deleni... az moc.
+    #coz vede celkove i k tomu ze kdyz test selze neni uplne jasne kde a co se stalo... z logu jsou jasne akce ale ne stranky napriklad... .
+    #errorovy vystup do comannd liny ti taky moc nepomuze... .
+    #a vlasten s emi tezko dohledavaji logy a chyb9 tam asi timestamp.
     loginPage.clickLoginBtn
 
 def addToCartTest():
--- ucastnici/Antonin/test_checkout_v3.py
+++ ucastnici/Antonin/test_checkout_v3.py
@@ -2,6 +2,9 @@ import logging
 from Assets.tests import setup, loginTest, addToCartTest, cartTest, checkoutTest, teardown
 
 result = "Test completed successfully!"
+#TODO - Lektor: vetsinu jsme komentovali na lekci. Takze moje komentare nemusi / nebudou uplne.
+#TODO - Lektor: nebal bych se uz na teto urovni tu mit nakou hlavni parametrizaci... jako treba jaka webova stranka se testuje.
+#TODO - Lektor: ty testovaci data uplne zmizeli na vsech urovnich. To neni dobre. Je fajn pouzite POM ale testovaci data cloveka zajimaji, jako heslo jmeno, ktery web... .
 
 #Test execution
 
