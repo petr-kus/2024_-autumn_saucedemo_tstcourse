6055823758aeeb54d7437310dc88d29681380981
Petr Kus <petr.kus@thermofisher.com>
2024-12-22 10:42:21 +0100

    DU6 - oprava - ondrej kucera + add to git ignore thinks

--- ucastnici/OndrejKucera/DU6.py
+++ ucastnici/OndrejKucera/DU6.py
@@ -7,6 +7,9 @@ from selenium.common.exceptions import NoSuchElementException
 import logging
 import time
 
+#TODO Lektor - komentovano na lekci takze jen prolitnu a okomentuji co tam vidim... .
+#TODO Lektor - nemusi / nebude to uplny seznam
+
 # Configure logging
 logging.basicConfig(filename='OK_log.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -17,13 +20,16 @@ def setup_driver():
     driver = webdriver.Chrome(options=options)
     return driver
 
+    #TODO Lektor - username_str mohl byt jen username
 def login(driver, username_str, password_str):
     """Log in to the application."""
     driver.get("https://www.saucedemo.com/")
     
     username = driver.find_element(By.ID, 'user-name')
     username.send_keys(username_str)
-    
+    #TODO Lektor - username_str mohl byt jen "one liner" driver.find_element(By.ID, 'user-name').send_keys(username_str)
+    #TODO Lektor - (By.ID, 'user-name') - slo oddelit a definovat bokem.
+
     password = driver.find_element(By.ID, 'password')
     password.send_keys(password_str)
     
@@ -31,6 +37,7 @@ def login(driver, username_str, password_str):
     login_button.click()
     
     logging.info('User logged in successfully.')
+    #TODO Lektor - tady mohlo byt overeni assertem ze se nacetla treba naka stranka - protoze tohle ti to vyhodi i kdyz se neprihlasis
 
 def add_to_cart(driver):
     """Add items to the cart."""
@@ -43,10 +50,12 @@ def add_to_cart(driver):
             add_to_cart_button = driver.find_element(By.ID, 'add-to-cart')
             add_to_cart_button.click()
             time.sleep(1)  # Wait for the action to complete
+            #TODO Lektor - waity jsou antipatern meli byt parametrizovany bokem
             back_to_main_page = driver.find_element(By.ID, 'back-to-products')
             back_to_main_page.click()
         except NoSuchElementException:
             logging.error(f'Could not find or interact with {item_id}.')
+            #TODO Lektor - super chvalim try and except a dobry log...
     
     # Check the shopping cart badge
     shopping_cart_badge = driver.find_element(By.CSS_SELECTOR, ".shopping_cart_badge[data-test='shopping-cart-badge']")
@@ -85,7 +94,8 @@ def logout(driver):
     """Log out from the application."""
     main_menu_icon = driver.find_element(By.ID, 'react-burger-menu-btn')
     main_menu_icon.click()
-    
+    #TODO Lektor - tady neni wait takze test selze... :-( aspon staticky wait tady mohl byt. I kdyz je to antipatern.
+
     logout_button = driver.find_element(By.ID, 'logout_sidebar_link')
     logout_button.click()
     
@@ -98,6 +108,7 @@ def logout(driver):
 
 def main():
     """Main function to execute the test."""
+     #TODO tady mohl byt global driver a nemuse jsi si to predavat ve funkcich
     driver = setup_driver()
     
     try:
@@ -105,9 +116,12 @@ def main():
         add_to_cart(driver)
         remove_from_cart(driver)
         logout(driver)
-        
+        #TODO Lektor - chvalim za krasne oddeleni struktury testu!
+        #TODO tady chybel driv konec exceptu!
     finally:
         driver.quit()
+    
+    
 
 if __name__ == "__main__":
     main()
