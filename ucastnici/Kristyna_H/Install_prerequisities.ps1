85f62d6656bf9802bf24d8c55f81ebb09c862ee3
Kristyna <141497130+krihri@users.noreply.github.com>
2024-12-22 16:41:26 +0100

    Update Install_prerequisities.ps1
    Student message

--- ucastnici/Kristyna_H/Install_prerequisities.ps1
+++ ucastnici/Kristyna_H/Install_prerequisities.ps1
@@ -3,6 +3,7 @@ python.exe -m pip install --upgrade pip
 python -m venv venv
 ./venv/Scripts/Activate.ps1
 #Lektor - na rakdu vyse jsem ti opravil activaci virtualniho prostredi... .
+# Student - jen pro info - ve windows opravena aktivace nefunguje. Funguje toto .\/.venv/Scripts/Activate.ps1
 pip install -r requirements.txt
 pip freeze > requirements.txt
 rfbrowser init
