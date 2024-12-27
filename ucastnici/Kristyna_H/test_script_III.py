bcf4b0fb50c33f463b7511daa63c7b4ac9bd0aa8
Petr Kus <petr.kus@thermofisher.com>
2024-12-27 18:54:09 +0100

    DU 7 - oprava - kristy

--- ucastnici/Kristyna_H/test_script_III.py
+++ ucastnici/Kristyna_H/test_script_III.py
@@ -1,25 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+#TODO Lektor - zbytecny nepouzity import (snazime se minimalizovat importy kazdy import je alokace, pametia zdrzeni...)
 from selenium.webdriver.common.by import By
 
 import time
+#TODO Lektor - zbytecny nepouzity import (snazime se minimalizovat importy kazdy import je alokace, pametia zdrzeni...)
 from selenium.webdriver.chrome.options import Options
 import logging
 
 import pytest
 
 logging.basicConfig(filename='pytest.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+#TODO Lektor - pochvala za time stamp.
 
+#TODO Lektor - ukol jen proletim a dam par komentaru. Vetsinu jsem odpovedel/komentoval na lekci.
+#TODO Lektor - Take pridavam priklad jak by to slo napsat s lektorksymi komentari, abych uzavrel tuto 7 ulohu kde by mel byt vrchol umeni jak test automatizovat bez Test Frameworku.
+#TODO Lektor - Priklad je v 1_lektor_petr_kus/Python_OOP_POM_Lectors_example/ i s komentarema co si z toho kde odnest... . A je tam pouzity pattern singleton pro predani driveru (odpoved na otazku z lekci ja to udelat dobre... )
 
 class LoginPage:
+    #TODO Lektor - username_field = (By.ID, "user-name")  
     def __init__(self, driver):
         self.driver = driver
+        #TODO Lektor - predani driveru zrovna v tomt testu mohlo byt pres Global Driver variable. 
+        #TODO Lektor - ale chvalim ze je pouzite OOP pro to a init. Ale lepsi by byl ten singlton pattern. 
+        #TODO Lektor - A kdyz uz je to takhle... mohlo to byt kazdy ten page object oddelen separe do souboru.
 
     def login(self, username, password):
         self.driver.find_element(By.ID, "user-name").send_keys(username)
         self.driver.find_element(By.ID, "password").send_keys(password)
         self.driver.find_element(By.ID, "login-button").click()
+        #TODO Lektor - (By.ID, "user-name") a pod. melo byt definovano nahore jako parametr. Viz priklad.
+        #self.driver.find_element(*self.username_field).send_keys(username)
         logging.info("User succesfuly logged in")
+        #TODO Lektor - nejspis je, ale jiste to nevis... tak to neloguj takto. Ale treba should nebo neco oc je jasne... .
 
 
 class InventoryPage:
@@ -42,7 +55,7 @@ class InventoryPage:
     def go_to_cart(self):
         self.driver.find_element(By.ID, "shopping_cart_container").click()
         logging.info("Going to cart.")
-        
+        #TODO Lektor - obecne se me libi ze vsude neco logujes a docela hezky. 
         
 class ProductDetailPage:
     def __init__(self, driver):
@@ -130,7 +143,10 @@ class OrderTest:
         self.checkout_page = CheckoutPage(driver)
         self.summary_page = SummaryPage(driver)
         self.final_page = FinalPage(driver)
-        self.login_page.login("standard_user", "secret_sauce")
+        self.login_page.login("standard_user", "secret_sauce") 
+        
+        #TODO: toto uz je test step a je to sem schovane... cekal bych to nekde viditelneji... .
+        #v pytestu by bylo faj  pro toto udelat zvlas fixture ... user_is_logged napriklad...
         
     def add_items_to_cart_directly_and_place_order(self, item_button_ids, expected_item_total_price):
         for idx, item_button_id in enumerate(item_button_ids):
@@ -193,6 +209,7 @@ def driver():
     options.add_argument("start-maximized")
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.saucedemo.com/")
+    #TODO Lektor - parametrizace by mela byt vyvedena ven...
     yield driver
     driver.close()
     driver.quit()
@@ -201,8 +218,14 @@ def driver():
 def test_adding_items_to_cart_directly_and_placing_order(driver):
     order_test = OrderTest(driver)
     order_test.add_items_to_cart_directly_and_place_order(["add-to-cart-test.allthethings()-t-shirt-(red)", "add-to-cart-sauce-labs-fleece-jacket"], 65.98)
+    #TODO Lektor - ty id by mela byt nekam schovana za pom... . a asi i ty ceny by meli byt nekam vyextrahovany mimo ... a ne napsany takto.
+    #TODO Lektor - jde cititi jak je to necitelne... .
+    #TODO Lektor - ty kroky co jsou schovane add_items_to_cart_directly_and_place_order by meli byt spis vvedene sem vcetne toho loginu... aby to tady bylo vic citelne. 
+    #TODO Lektor - jinak se to tu jen prevolava ale nema to pridanou hodnotu citelnosti testu... .
 
 
 def test_adding_items_to_cart_via_detail_page_and_placing_order(driver):
     order_test = OrderTest(driver)
     order_test.add_items_to_cart_via_detail_page_and_place_order(["item_1_title_link", "item_4_title_link", "item_0_title_link"], 55.97)
+
+ #TODO Lektor - chvalim ze uz zde je pouzit pytest!
